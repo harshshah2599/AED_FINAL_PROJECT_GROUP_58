ff1114c1811e92752b16dc9bb1cd5be866259e65
Aditi-jajoo <114697776+Aditi-jajoo@users.noreply.github.com>
2022-11-30 14:01:29 -0500

    useraccount java file has been created

---
+++ src/model/UserAccount/loggedInUser.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.UserAccount;
+
+/**
+ *
+ * @author aditi
+ */
+
+public class loggedInUser {
+    private int logInId;
+
+    public int getLogInId() {
+        return logInId;
+    }
+
+    public void setLogInId(int logInId) {
+        this.logInId = logInId;
+    }
+}
